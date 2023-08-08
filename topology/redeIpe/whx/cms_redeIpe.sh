ae5329573efddced4b963d80844f79d01373786d
Lucas Borges de Oliveira <lbo.comp@gmail.com>
2023-08-07 22:32:24 -0300

    ajustes

--- topology/redeIpe/whx/cms_redeIpe.sh
+++ topology/redeIpe/whx/cms_redeIpe.sh
@@ -16,3 +16,6 @@ docker exec clab-whx-redeIpe-pi-ht bash -c "ip addr add 10.0.6.1/24 dev eth1 ; i
 docker exec clab-whx-redeIpe-ce-ht bash -c "ip addr add 10.0.7.1/24 dev eth1 ; ip route add 10.0.2.0/24 dev eth1 ; ip route add 10.0.3.0/24 dev eth1 ; ip route add 10.0.4.0/24 dev eth1 ; ip route add 10.0.5.0/24 dev eth1 ; ip route add 10.0.6.0/24 dev eth1 ; ip route add 10.0.7.0/24 dev eth1"
 
 #-------
+
+
+docker exec clab-whx-redeIpe-df-ht ping -c 1 10.0.2.254 ; docker exec clab-whx-redeIpe-to-ht ping -c 1 10.0.3.254 ; docker exec clab-whx-redeIpe-pa-ht ping -c 1 10.0.4.254 ; docker exec clab-whx-redeIpe-ma-ht ping -c 1 10.0.5.254 ; docker exec clab-whx-redeIpe-pi-ht ping -c 1 10.0.6.254 ; docker exec clab-whx-redeIpe-ce-ht ping -c 1 10.0.7.254
